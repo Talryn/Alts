6cc96268d97f797cc0042a18cd0c3825424ac50d
Talryn <git@talryn.sent.com>
2012-06-11 22:13:11 -0400

    Update guild export status format phrase key

--- Alts.lua
+++ Alts.lua
@@ -1713,7 +1713,7 @@ function Alts:ShowGuildExportFrame()
                 self.db.profile.exportUseAlts,
                 self.db.profile.exportEscape
             )
-			local exportStatusFmt = L["GuildExportStatusFormat"] 
+			local exportStatusFmt = L["GuildExport_StatusFormat"] 
 			frame:SetStatusText(exportStatusFmt:format(numChars))
             frame.multiline:SetText(guildExportText)
         end)
