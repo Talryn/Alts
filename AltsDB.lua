77e450044d790b682eb90f7e5c2f4f99fdd089de
Talryn <git@talryn.sent.com>
2021-02-23 13:44:32 -0500

    Move realm names to separate file.

--- AltsDB.lua
+++ AltsDB.lua
@@ -3,6 +3,7 @@ local ADDON_NAME, addon = ...
 
 local AltsDB = {}
 addon.AltsDB = AltsDB
+local realmNames = addon.realmNames
 
 -- Use local versions of standard LUA items for performance
 local string = _G.string
@@ -33,75 +34,6 @@ local function ReverseTable(table)
   end
   return reverse
 end
-
-
-local realmNames = {
-    ["Aeriepeak"] = "AeriePeak",
-    ["Altarofstorms"] = "AltarofStorms",
-    ["Alteracmountains"] = "AlteracMountains",
-    ["Aman'thul"] = "Aman'Thul",
-    ["Argentdawn"] = "ArgentDawn",
-    ["Azjolnerub"] = "AzjolNerub",
-    ["Blackdragonflight"] = "BlackDragonflight",
-    ["Blackwaterraiders"] = "BlackwaterRaiders",
-    ["Blackwinglair"] = "BlackwingLair",
-    ["Blade'sedge"] = "Blade'sEdge",
-    ["Bleedinghollow"] = "BleedingHollow",
-    ["Bloodfurnace"] = "BloodFurnace",
-    ["Bloodsailbuccaneers"] = "BloodsailBuccaneers",
-    ["Boreantundra"] = "BoreanTundra",
-    ["Burningblade"] = "BurningBlade",
-    ["Burninglegion"] = "BurningLegion",
-    ["Cenarioncircle"] = "CenarionCircle",
-    ["Darkiron"] = "DarkIron",
-    ["Darkmoonfaire"] = "DarkmoonFaire",
-    ["Dath'remar"] = "Dath'Remar",
-    ["Demonsoul"] = "DemonSoul",
-    ["Drak'tharon"] = "Drak'Tharon",
-    ["Earthenring"] = "EarthenRing",
-    ["Echoisles"] = "EchoIsles",
-    ["Eldre'thalas"] = "Eldre'Thalas",
-    ["Emeralddream"] = "EmeraldDream",
-    ["Grizzlyhills"] = "GrizzlyHills",
-    ["Jubei'thos"] = "Jubei'Thos",
-    ["Kel'thuzad"] = "Kel'Thuzad",
-    ["Khazmodan"] = "KhazModan",
-    ["Kirintor"] = "KirinTor",
-    ["Kultiras"] = "KulTiras",
-    ["Laughingskull"] = "LaughingSkull",
-    ["Lightning'sblade"] = "Lightning'sBlade",
-    ["Mal'ganis"] = "Mal'Ganis",
-    ["Mok'nathal"] = "Mok'Nathal",
-    ["Moonguard"] = "MoonGuard",
-    ["Quel'thalas"] = "Quel'Thalas",
-    ["Scarletcrusade"] = "ScarletCrusade",
-    ["Shadowcouncil"] = "ShadowCouncil",
-    ["Shatteredhalls"] = "ShatteredHalls",
-    ["Shatteredhand"] = "ShatteredHand",
-    ["Silverhand"] = "SilverHand",
-    ["Sistersofelune"] = "SistersofElune",
-    ["Steamwheedlecartel"] = "SteamwheedleCartel",
-    ["Theforgottencoast"] = "TheForgottenCoast",
-    ["Thescryers"] = "TheScryers",
-    ["Theunderbog"] = "TheUnderbog",
-    ["Theventureco"] = "TheVentureCo",
-    ["Thoriumbrotherhood"] = "ThoriumBrotherhood",
-    ["Tolbarad"] = "TolBarad",
-    ["Twistingnether"] = "TwistingNether",
-    ["Wyrmrestaccord"] = "WyrmrestAccord",
-    -- Classic realms
-    ["Arcanitereaper"] = "ArcaniteReaper",
-    ["Deviatedelight"] = "DeviateDelight",
-    ["Oldblanchy"] = "OldBlanchy",
-    -- Classic EU realms
-    ["Pyrewoodvillage"] = "PyrewoodVillage",
-    ["Mirageraceway"] = "MirageRaceway",
-    ["Zandalartribe"] = "ZandalarTribe",
-    ["Nethergardekeep"] = "NethergardeKeep",
-    ["Dragon'scall"] = "Dragon'sCall",
-    ["Hydraxianwaterlords"] = "HydraxianWaterlords",
-    ["Tenstorms"] = "TenStorms",
-}
 
 local MULTIBYTE_FIRST_CHAR = "^([\192-\255]?%a?[\128-\191]*)"
 
